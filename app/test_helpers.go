91928e21b4977ce955a24f724f5b83b77a4f407e
Jacob Gadikian <jacobgadikian@gmail.com>
2023-02-08 18:32:39 +0700

    added nolints and todos to fix them for wrapped errors

--- app/test_helpers.go
+++ app/test_helpers.go
@@ -480,7 +480,7 @@ func NewPubKeyFromHex(pk string) (res cryptotypes.PubKey) {
 		panic(err)
 	}
 	if len(pkBytes) != ed25519.PubKeySize {
-		panic(errors.Wrap(errors.ErrInvalidPubKey, "invalid pubkey size"))
+		panic(errors.Wrap(errors.ErrInvalidPubKey, "invalid pubkey size")) //nolint:staticcheck // ignore SA1019 // TODO: migrate to cosmossdk.io/errors
 	}
 	return &ed25519.PubKey{Key: pkBytes}
 }
