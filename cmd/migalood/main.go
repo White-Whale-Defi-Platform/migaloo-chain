6ef9623dd00848b8e978a420c25a0bfa2e28218d
Jacob Gadikian <jacobgadikian@gmail.com>
2023-02-10 22:35:18 +0700

    link the cmd folder to the rest of the app

--- cmd/migalood/main.go
+++ cmd/migalood/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"os"
 
-	"github.com/cosmos/cosmos-sdk/server"
-	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/White-Whale-Defi-Platform/migaloo-chain/app"
 	"github.com/White-Whale-Defi-Platform/migaloo-chain/cmd/migalood/cmd"
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 )
 
 func main() {
-	rootCmd, _ := NewRootCmd()
+	rootCmd, _ := cmd.NewRootCmd()
 
 	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
