d09c235e39e14555d15114657e6bf4a93083c1c4
Jacob Gadikian <jacobgadikian@gmail.com>
2023-02-13 18:33:07 +0700

    allow more inbound peers

--- cmd/migalood/cmd/root.go
+++ cmd/migalood/cmd/root.go
@@ -94,8 +94,8 @@ func initTendermintConfig() *tmcfg.Config {
 	cfg := tmcfg.DefaultConfig()
 
 	// these values put a higher strain on node memory
-	// cfg.P2P.MaxNumInboundPeers = 100
-	// cfg.P2P.MaxNumOutboundPeers = 40
+	cfg.P2P.MaxNumInboundPeers = 320
+	cfg.P2P.MaxNumOutboundPeers = 40
 
 	return cfg
 }
