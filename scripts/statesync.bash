132b0cd2a2e741077de661fb87ddc94266804797
Jacob Gadikian <jacobgadikian@gmail.com>
2023-03-16 15:07:20 +0700

    fix urls

--- scripts/statesync.bash
+++ scripts/statesync.bash
@@ -23,14 +23,12 @@ migalood init test
 # get genesis
 wget -O ~/.migalood/config/genesis.json https://github.com/White-Whale-Defi-Platform/migaloo-chain/raw/release/v2.0.x/networks/mainnet/genesis.json
 
-# Set minimum gas price.
-sed -i'' 's/minimum-gas-prices = ""/minimum-gas-prices = "0.0025uwhale"/' $HOME/.migalood/config/app.toml
 
 # Get "trust_hash" and "trust_height".
 INTERVAL=1000
-LATEST_HEIGHT=$(curl -s https://https://rpc-whitewhale.goldenratiostaking.net/block | jq -r .result.block.header.height)
+LATEST_HEIGHT=$(curl -s https://whitewhale-rpc.lavenderfive.com:443/block | jq -r .result.block.header.height)
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL)) 
-TRUST_HASH=$(curl -s "https://https://rpc-whitewhale.goldenratiostaking.net/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+TRUST_HASH=$(curl -s "https://whitewhale-rpc.lavenderfive.com:443/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
 # Print out block and transaction hash from which to sync state.
 echo "trust_height: $BLOCK_HEIGHT"
@@ -39,12 +37,12 @@ echo "trust_hash: $TRUST_HASH"
 # Export state sync variables.
 export MIGALOOD_STATESYNC_ENABLE=true
 export MIGALOOD_P2P_MAX_NUM_OUTBOUND_PEERS=200
-export MIGALOOD_STATESYNC_RPC_SERVERS="https://https://whitewhale-rpc.lavenderfive.com:443,https://rpc-whitewhale.goldenratiostaking.net:443"
+export MIGALOOD_STATESYNC_RPC_SERVERS="https://whitewhale-rpc.lavenderfive.com:443,https://rpc-whitewhale.goldenratiostaking.net:443"
 export MIGALOOD_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
 export MIGALOOD_STATESYNC_TRUST_HASH=$TRUST_HASH
 
 # Fetch and set list of seeds from chain registry.
-export MIGALOOD_P2P_SEEDS=$(curl -s https://raw.githubusercontent.com/cosmos/chain-registry/master/cosmoshub/chain.json | jq -r '[foreach .peers.seeds[] as $item (""; "\($item.id)@\($item.address)")] | join(",")')
+export MIGALOOD_P2P_SEEDS=$(curl -s https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/chain.json | jq -r '[foreach .peers.seeds[] as $item (""; "\($item.id)@\($item.address)")] | join(",")')
 
 # Start chain.
-migalood start --x-crisis-skip-assert-invariants
+migalood start --x-crisis-skip-assert-invariants --minimum-gas-prices 0.00001uwhale
