47b901a114717953ab6493f36d2ff8aacbb37748
catShaark <gundamaster5@gmail.com>
2023-07-19 21:29:12 +0700

    fix: upgrade handler and store loader

---
+++ app/upgrades/v2_2_5/upgrades.go
@@ -0,0 +1,19 @@
+package v2_2_5
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+// We set the app version to pre-upgrade because it will be incremented by one
+// after the upgrade is applied by the handler.
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
