9ed2dce341705f4856dc6e74e785b06dfd4a1b24
vuong177 <nguyenvuong1122000@gmail.com>
2023-07-19 20:34:31 +0700

    upgrade name

--- app/upgrades/v2/constants.go
+++ app/upgrades/v2/constants.go
@@ -1,22 +1,18 @@
 package v2
 
 import (
-	alliancetypes "github.com/terra-money/alliance/x/alliance/types"
-
 	store "github.com/cosmos/cosmos-sdk/store/types"
 
 	"github.com/White-Whale-Defi-Platform/migaloo-chain/v3/app/upgrades"
 )
 
 // UpgradeName defines the on-chain upgrade name for the Migaloo v2 upgrade.
-const UpgradeName = "v2"
+const UpgradeName = "v2.2.5"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{
-			alliancetypes.ModuleName,
-		},
+		Added: []string{},
 	},
 }
