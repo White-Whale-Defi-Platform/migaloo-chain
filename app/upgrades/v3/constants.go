5db5e61efa8713d494815309d1a94d1d9ea3912b
Jacob Gadikian <jacobgadikian@gmail.com>
2023-09-20 18:05:03 +0000

    Update constants.go

--- app/upgrades/v3/constants.go
+++ app/upgrades/v3/constants.go
@@ -6,7 +6,7 @@ import (
 	ibchookstypes "github.com/terra-money/core/v2/x/ibc-hooks/types"
 )
 
-// UpgradeName defines the on-chain upgrade name for the Migaloo v2 upgrade.
+// UpgradeName defines the on-chain upgrade name for the Migaloo v3 upgrade.
 const UpgradeName = "v3.0.0"
 
 var Upgrade = upgrades.Upgrade{
