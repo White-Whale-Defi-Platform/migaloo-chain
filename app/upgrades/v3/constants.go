89ce3a7efd5e92a244d625c30e0fd8a2ba166808
Jacob Gadikian <jacobgadikian@gmail.com>
2023-09-20 17:19:56 +0000

    Update constants.go

--- app/upgrades/v3/constants.go
+++ app/upgrades/v3/constants.go
@@ -7,7 +7,7 @@ import (
 )
 
 // UpgradeName defines the on-chain upgrade name for the Migaloo v2 upgrade.
-const UpgradeName = "v2.2.5"
+const UpgradeName = "v3.0.0"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
