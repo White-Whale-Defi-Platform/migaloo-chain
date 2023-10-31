8eed6691206c824444be43ab7c41a20d9481c3be
Lặc <expertdicer@gmail.com>
2023-10-31 12:04:23 +0700

    add globalfee ante

---
+++ ante/ante.go
@@ -0,0 +1,85 @@
+package ante
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
+	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	ibcante "github.com/cosmos/ibc-go/v6/modules/core/ante"
+	ibckeeper "github.com/cosmos/ibc-go/v6/modules/core/keeper"
+
+	migaloofeeante "github.com/White-Whale-Defi-Platform/migaloo-chain/v3/x/globalfee/ante"
+)
+
+// HandlerOptions extend the SDK's AnteHandler options by requiring the IBC
+// channel keeper.
+type HandlerOptions struct {
+	ante.HandlerOptions
+	Codec                  codec.BinaryCodec
+	GovKeeper              *govkeeper.Keeper
+	IBCkeeper              *ibckeeper.Keeper
+	ExtensionOptionChecker authante.ExtensionOptionChecker
+	BypassMinFeeMsgTypes   []string
+	GlobalFeeSubspace      paramtypes.Subspace
+	StakingSubspace        paramtypes.Subspace
+	TxFeeChecker           authante.TxFeeChecker
+}
+
+func NewAnteHandler(opts HandlerOptions) (sdk.AnteHandler, error) {
+	if opts.AccountKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "account keeper is required for AnteHandler")
+	}
+	if opts.BankKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "bank keeper is required for AnteHandler")
+	}
+	if opts.SignModeHandler == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for AnteHandler")
+	}
+	if opts.IBCkeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "IBC keeper is required for AnteHandler")
+	}
+	if opts.GlobalFeeSubspace.Name() == "" {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "globalfee param store is required for AnteHandler")
+	}
+	if opts.StakingSubspace.Name() == "" {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "staking param store is required for AnteHandler")
+	}
+	if opts.GovKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "gov keeper is required for AnteHandler")
+	}
+
+	sigGasConsumer := opts.SigGasConsumer
+	if sigGasConsumer == nil {
+		sigGasConsumer = ante.DefaultSigVerificationGasConsumer
+	}
+
+	// maxBypassMinFeeMsgGasUsage is the maximum gas usage per message
+	// so that a transaction that contains only message types that can
+	// bypass the minimum fee can be accepted with a zero fee.
+	// For details, see gaiafeeante.NewFeeDecorator()
+	var maxBypassMinFeeMsgGasUsage uint64 = 200_000
+
+	anteDecorators := []sdk.AnteDecorator{
+		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		ante.NewExtensionOptionsDecorator(opts.ExtensionOptionChecker),
+		ante.NewValidateBasicDecorator(),
+		ante.NewTxTimeoutHeightDecorator(),
+		ante.NewValidateMemoDecorator(opts.AccountKeeper),
+		ante.NewConsumeGasForTxSizeDecorator(opts.AccountKeeper),
+		NewGovPreventSpamDecorator(opts.Codec, opts.GovKeeper),
+		migaloofeeante.NewFeeDecorator(opts.BypassMinFeeMsgTypes, opts.GlobalFeeSubspace, opts.StakingSubspace, maxBypassMinFeeMsgGasUsage),
+
+		ante.NewDeductFeeDecorator(opts.AccountKeeper, opts.BankKeeper, opts.FeegrantKeeper, opts.TxFeeChecker),
+		ante.NewSetPubKeyDecorator(opts.AccountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewValidateSigCountDecorator(opts.AccountKeeper),
+		ante.NewSigGasConsumeDecorator(opts.AccountKeeper, sigGasConsumer),
+		ante.NewSigVerificationDecorator(opts.AccountKeeper, opts.SignModeHandler),
+		ante.NewIncrementSequenceDecorator(opts.AccountKeeper),
+		ibcante.NewRedundantRelayDecorator(opts.IBCkeeper),
+	}
+
+	return sdk.ChainAnteDecorators(anteDecorators...), nil
+}
---
+++ ante/gov_ante_test.go
@@ -0,0 +1,92 @@
+package ante_test
+
+import (
+	"fmt"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/White-Whale-Defi-Platform/migaloo-chain/v3/ante"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	migalooapp "github.com/White-Whale-Defi-Platform/migaloo-chain/v3/app"
+)
+
+var (
+	insufficientCoins = sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 100))
+	minCoins          = sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 1000000))
+	moreThanMinCoins  = sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 2500000))
+	testAddr          = sdk.AccAddress("test1")
+)
+
+type GovAnteHandlerTestSuite struct {
+	suite.Suite
+
+	app       *migalooapp.MigalooApp
+	ctx       sdk.Context
+	clientCtx client.Context
+}
+
+func (s *GovAnteHandlerTestSuite) SetupTest() {
+	app := migalooapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{
+		ChainID: fmt.Sprintf("test-chain-%s", tmrand.Str(4)),
+		Height:  1,
+	})
+
+	encodingConfig := migalooapp.MakeEncodingConfig()
+	encodingConfig.Amino.RegisterConcrete(&testdata.TestMsg{}, "testdata.TestMsg", nil)
+	testdata.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
+	s.app = app
+	s.ctx = ctx
+	s.clientCtx = client.Context{}.WithTxConfig(encodingConfig.TxConfig)
+}
+
+func TestGovSpamPreventionSuite(t *testing.T) {
+	suite.Run(t, new(GovAnteHandlerTestSuite))
+}
+
+func (s *GovAnteHandlerTestSuite) TestGlobalFeeMinimumGasFeeAnteHandler() {
+	// setup test
+	s.SetupTest()
+	tests := []struct {
+		title, description string
+		proposalType       string
+		proposerAddr       sdk.AccAddress
+		initialDeposit     sdk.Coins
+		expectPass         bool
+	}{
+		{"Passing proposal 1", "the purpose of this proposal is to pass", govv1beta1.ProposalTypeText, testAddr, minCoins, true},
+		{"Passing proposal 2", "the purpose of this proposal is to pass with more coins than minimum", govv1beta1.ProposalTypeText, testAddr, moreThanMinCoins, true},
+		{"Failing proposal", "the purpose of this proposal is to fail", govv1beta1.ProposalTypeText, testAddr, insufficientCoins, false},
+	}
+
+	decorator := ante.NewGovPreventSpamDecorator(s.app.AppCodec(), &s.app.GovKeeper)
+
+	for _, tc := range tests {
+		content, ok := govv1beta1.ContentFromProposalType(tc.title, tc.description, tc.proposalType)
+		s.Require().True(ok)
+		s.Require().NotNil(content)
+
+		msg, err := govv1beta1.NewMsgSubmitProposal(
+			content,
+			tc.initialDeposit,
+			tc.proposerAddr,
+		)
+
+		s.Require().NoError(err)
+
+		err = decorator.ValidateGovMsgs(s.ctx, []sdk.Msg{msg})
+		if tc.expectPass {
+			s.Require().NoError(err, "expected %v to pass", tc.title)
+		} else {
+			s.Require().Error(err, "expected %v to fail", tc.title)
+		}
+	}
+}
