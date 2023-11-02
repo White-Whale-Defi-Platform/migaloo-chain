eced8c4968b56a99450f3a9de70dad533d2bfcec
Hieu Vu <gundamaster5@gmail.com>
2023-11-02 12:28:21 +0700

    add upgrade handler for v3.0.2

---
+++ app/upgrades/v3_0_2/upgrades.go
@@ -0,0 +1,17 @@
+package v3
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+// small security fix, can be a no-op, running mm.RunMigarions just to be sure
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
