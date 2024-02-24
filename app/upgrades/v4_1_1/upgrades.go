e95670faea50b5469bc753503d23a324870051c0
Jacob Gadikian <jacobgadikian@gmail.com>
2024-02-24 23:20:26 +0200

    modify upgrade due to Khanh, Vuong, and Long having backed out of their stated agreement twice.

--- app/upgrades/v4_1_1/upgrades.go
+++ app/upgrades/v4_1_1/upgrades.go
@@ -1,12 +1,6 @@
 package v4
 
 import (
-	"fmt"
-	"time"
-
-	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
-
-	"cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -24,7 +18,7 @@ import (
 	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
 )
 
-// CreateUpgradeHandler small security fix, can be a no-op, running mm.RunMigarions just to be sure
+// CreateUpgradeHandler that migrates the chain from v3.0.2 to v4.1.1
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
@@ -33,8 +27,8 @@ func CreateUpgradeHandler(
 	consensusParamsKeeper consensuskeeper.Keeper,
 	icacontrollerKeeper icacontrollerkeeper.Keeper,
 	accountKeeper authkeeper.AccountKeeper,
-	stakingKeeper stakingKeeper.Keeper,
-	bankKeeper bankKeeper.Keeper,
+	_ stakingKeeper.Keeper,
+	_ bankKeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		// READ: https://github.com/cosmos/cosmos-sdk/blob/v0.47.4/UPGRADING.md#xconsensus
@@ -56,149 +50,6 @@ func CreateUpgradeHandler(
 		moduleAcc.Permissions = []string{authtypes.Burner}
 		accountKeeper.SetModuleAccount(ctx, moduleAcc)
 
-		migrateMultisigVesting(ctx, stakingKeeper, bankKeeper, accountKeeper)
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
-}
-
-// migrateMultisigVesting moves the unvested from the ContinuousVestingAccount -> the new ContinuousVestingAccount
-// - Retrieves the old multisig vesting account
-// - Instantly finish all redelegations, then unbond all tokens.
-// - Transfer unvested token to the new multisign ContinuousVestingAccount
-func migrateMultisigVesting(ctx sdk.Context,
-	stakingKeeper stakingKeeper.Keeper,
-	bankKeeper bankKeeper.Keeper,
-	accountKeeper authkeeper.AccountKeeper,
-) {
-	currentAddr := sdk.MustAccAddressFromBech32(NotionalMultisigVestingAccount)
-	newAddr := sdk.MustAccAddressFromBech32(NewNotionalMultisigAccount)
-
-	currentAcc := accountKeeper.GetAccount(ctx, currentAddr)
-
-	currentVestingAcc, ok := currentAcc.(*vestingtypes.ContinuousVestingAccount)
-	if !ok {
-		// skip if account invalid
-		fmt.Printf("err currentAcc.(*vestingtypes.ContinuousVestingAccount): %+v\n", currentAcc)
-		return
-	}
-	// process migrate
-	processMigrateMultisig(ctx, stakingKeeper, bankKeeper, accountKeeper, currentAddr, newAddr, currentVestingAcc)
-}
-
-func processMigrateMultisig(ctx sdk.Context, stakingKeeper stakingKeeper.Keeper,
-	bankKeeper bankKeeper.Keeper,
-	accountKeeper authkeeper.AccountKeeper,
-	currentAddr, newAddr sdk.AccAddress,
-	oldAcc *vestingtypes.ContinuousVestingAccount,
-) {
-	redelegated, err := completeAllRedelegations(ctx, ctx.BlockTime(), stakingKeeper, currentAddr)
-	if err != nil {
-		panic(err)
-	}
-
-	unbonded, err := unbondAllAndFinish(ctx, ctx.BlockTime(), stakingKeeper, currentAddr)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("currentAddr Instant Redelegations: %s\n", redelegated)
-	fmt.Printf("currentAddr Instant Unbonding: %s\n", unbonded)
-
-	// get vesting balance
-	vestingCoin := oldAcc.GetVestingCoins(ctx.BlockTime())
-	fmt.Printf("Total vesting balance %v\n", vestingCoin)
-	if vestingCoin.Empty() {
-		panic(err)
-	}
-
-	// setup NewContinuousVestingAccount
-	newAccount := authtypes.NewBaseAccountWithAddress(newAddr)
-	newVestingAcc := vestingtypes.NewContinuousVestingAccount(newAccount, vestingCoin,
-		ctx.BlockTime().Unix(), oldAcc.EndTime)
-	accountKeeper.SetAccount(ctx, newVestingAcc)
-
-	// end current ContinuousVestingAccount
-	oldAcc.OriginalVesting = oldAcc.OriginalVesting.Sub(vestingCoin[0])
-	oldAcc.DelegatedFree = sdk.NewCoins()
-	oldAcc.DelegatedVesting = sdk.NewCoins()
-	oldAcc.EndTime = ctx.BlockTime().Unix()
-	accountKeeper.SetAccount(ctx, oldAcc)
-
-	// move vesting coin to new ContinuousVestingAccount
-	err = bankKeeper.SendCoins(ctx, currentAddr, newAddr, vestingCoin)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func GetVestingCoin(ctx sdk.Context, acc *vestingtypes.ContinuousVestingAccount) (unvested math.Int) {
-	vestingCoin := acc.GetVestingCoins(ctx.BlockTime())
-	return vestingCoin[0].Amount
-}
-
-func completeAllRedelegations(ctx sdk.Context, now time.Time,
-	stakingKeeper stakingKeeper.Keeper,
-	accAddr sdk.AccAddress,
-) (math.Int, error) {
-	redelegatedAmt := math.ZeroInt()
-
-	for _, activeRedelegation := range stakingKeeper.GetRedelegations(ctx, accAddr, 65535) {
-		redelegationSrc, _ := sdk.ValAddressFromBech32(activeRedelegation.ValidatorSrcAddress)
-		redelegationDst, _ := sdk.ValAddressFromBech32(activeRedelegation.ValidatorDstAddress)
-
-		// set all entry completionTime to now so we can complete re-delegation
-		for i := range activeRedelegation.Entries {
-			activeRedelegation.Entries[i].CompletionTime = now
-			redelegatedAmt = redelegatedAmt.Add(math.Int(activeRedelegation.Entries[i].SharesDst))
-		}
-
-		stakingKeeper.SetRedelegation(ctx, activeRedelegation)
-		_, err := stakingKeeper.CompleteRedelegation(ctx, accAddr, redelegationSrc, redelegationDst)
-		if err != nil {
-			return redelegatedAmt, err
-		}
-	}
-
-	return redelegatedAmt, nil
-}
-
-func unbondAllAndFinish(ctx sdk.Context, now time.Time,
-	stakingKeeper stakingKeeper.Keeper,
-	accAddr sdk.AccAddress,
-) (math.Int, error) {
-	unbondedAmt := math.ZeroInt()
-
-	// Unbond all delegations from the account
-	for _, delegation := range stakingKeeper.GetAllDelegatorDelegations(ctx, accAddr) {
-		validatorValAddr := delegation.GetValidatorAddr()
-		_, found := stakingKeeper.GetValidator(ctx, validatorValAddr)
-		if !found {
-			continue
-		}
-
-		_, err := stakingKeeper.Undelegate(ctx, accAddr, validatorValAddr, delegation.GetShares())
-		if err != nil {
-			return math.ZeroInt(), err
-		}
-	}
-
-	// Take all unbonding and complete them.
-	for _, unbondingDelegation := range stakingKeeper.GetAllUnbondingDelegations(ctx, accAddr) {
-		validatorStringAddr := unbondingDelegation.ValidatorAddress
-		validatorValAddr, _ := sdk.ValAddressFromBech32(validatorStringAddr)
-
-		// Complete unbonding delegation
-		for i := range unbondingDelegation.Entries {
-			unbondingDelegation.Entries[i].CompletionTime = now
-			unbondedAmt = unbondedAmt.Add(unbondingDelegation.Entries[i].Balance)
-		}
-
-		stakingKeeper.SetUnbondingDelegation(ctx, unbondingDelegation)
-		_, err := stakingKeeper.CompleteUnbonding(ctx, accAddr, validatorValAddr)
-		if err != nil {
-			return math.ZeroInt(), err
-		}
-	}
-
-	return unbondedAmt, nil
 }
