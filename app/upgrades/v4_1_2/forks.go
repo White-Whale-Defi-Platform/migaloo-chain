bf1970f6e7bdf7eeec3693f982b9f9dc729fbf77
Khanh Hoa <hoa@notional.ventures>
2024-03-04 11:51:33 +0700

    chore: refactor

--- app/upgrades/v4_1_2/forks.go
+++ app/upgrades/v4_1_2/forks.go
@@ -10,7 +10,6 @@ import (
 func UpdateAlliance(ctx sdk.Context, alk alliancekeeper.Keeper) {
 	allianceParams := alk.GetParams(ctx)
 	allianceParams.RewardDelayTime = 10 * time.Minute
-
 	_ = alk.SetParams(ctx, allianceParams)
 
 	asset, found := alk.GetAssetByDenom(ctx, "ibc/30E9709461C4DA26A7A579E11DE44B591E676DB1B7F94714FBFF87ED6E47D6F4")
